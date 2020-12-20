ac0f167ac2d18de474a962f5124c5ba20c84a55d
tareq sanabra <tareq.sanabra@gmail.com>
2020-12-20 20:52:23 +0100

    up

---
+++ packages/form_bloc/lib/src/blocs/image_field/image_field_state.dart
@@ -0,0 +1,83 @@
+import 'package:meta/meta.dart';
+import 'package:quiver/core.dart';
+import '../../../form_bloc.dart';
+
+class ImageFieldBlocState<ExtraData>
+    extends FieldBlocState<String, String, ExtraData> {
+  ImageFieldBlocState({
+    @required String value,
+    @required String error,
+    @required bool isInitial,
+    @required Suggestions<String> suggestions,
+    @required bool isRequired,
+    @required bool isValidated,
+    @required bool isValidating,
+    FormBloc formBloc,
+    @required String name,
+    List additionalProps = const <dynamic>[],
+    dynamic Function(String value) toJson,
+    ExtraData extraData,
+  }) : super(
+          value: value,
+          error: error,
+          isInitial: isInitial,
+          suggestions: suggestions,
+          isValidated: isValidated,
+          isValidating: isValidating,
+          isRequired: isRequired,
+          formBloc: formBloc,
+          name: name,
+          toJson: toJson,
+          extraData: extraData,
+        );
+
+  /// Parse the [value] to [int].
+  /// if the value is an [int] returns an [int],
+  /// else returns `null`.
+  int get valueToInt => int.tryParse(value);
+
+  /// Parse the [value] to [double].
+  /// if the value is a [double] returns a [double],
+  /// else returns `null`.
+  double get valueToDouble => double.tryParse(value);
+
+  @override
+  ImageFieldBlocState<ExtraData> copyWith({
+    Optional<String> value,
+    Optional<String> error,
+    bool isInitial,
+    Optional<Suggestions<String>> suggestions,
+    bool isValidated,
+    bool isRequired,
+    bool isValidating,
+    FormBloc formBloc,
+    Optional<ExtraData> extraData,
+  }) {
+    return ImageFieldBlocState(
+      value: value == null ? this.value : value.orNull,
+      error: error == null ? this.error : error.orNull,
+      isInitial: isInitial ?? this.isInitial,
+      suggestions: suggestions == null ? this.suggestions : suggestions.orNull,
+      isValidated: isValidated ?? this.isValidated,
+      isValidating: isValidating ?? this.isValidating,
+      formBloc: formBloc ?? this.formBloc,
+      isRequired: isRequired ?? this.isRequired ?? false,
+      name: name,
+      //toJson: _toJson,
+      extraData: extraData == null ? this.extraData : extraData.orNull,
+    );
+  }
+
+  @override
+  List<Object> get props => [
+        value,
+        error,
+        isInitial,
+        suggestions,
+        isValidated,
+        isValidating,
+        extraData,
+        isRequired,
+        formBloc,
+      ];
+}
